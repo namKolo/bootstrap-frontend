8a07c1bffdb32331848717dd1b077641cd07399b
namKolo <pokemon.namkolo@gmail.com>
2019-12-07 14:13:15 +0700

    Add sample page to display github users

---
+++ src/pages/sample/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// store
+import { fetchGithubUsers, GithubUser } from 'store/sampleReducer';
+import { IApplicationState } from 'store';
+
+const SamplePage = () => {
+  const dispatch = useDispatch();
+  const result = useSelector((state: IApplicationState) => state.sample);
+
+  useEffect(() => {
+    if (!result.loaded && !result.loading) {
+      dispatch(fetchGithubUsers());
+    }
+  }, [result, dispatch]);
+
+  return (
+    <div>
+      {result.loading && <div>Loading...</div>}
+      {result.loaded && (
+        <div>
+          {result.data.map(user => (
+            <User user={user} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SamplePage;
+
+interface UserProps {
+  user: GithubUser;
+}
+const User: React.FC<UserProps> = ({ user }) => {
+  return <div>{user.login}</div>;
+};
